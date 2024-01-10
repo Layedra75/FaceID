a2d6dbb37bc8baa3be153621030d437bc34b379b
Layedra75 <layedra75@gmail.com>
2024-01-10 11:33:05 -0500

    com

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def process_request():
     return jsonify(extract_and_match_frames(img_url, vid_url))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
